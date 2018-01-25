a3da38a92b49e0bb5563cbe0568589b175ebdbd2
Andrew Tu <drewtu2@yahoo.com>
2018-01-25 14:19:22 -0500

    working driver, relevant setup scripts/makefile

--- lab1/launch_lcm.sh
+++ lab1/launch_lcm.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 #sudo chmod 666 /dev/ttyUSB0
-export CLASSPATH=$PWD
+export CLASSPATH=$PWD/my_types.jar
 #lcm-logger --auto-split-hours=1 -s ./data/lcm-log-%F-%T & 
 lcm-logger -s ./log/lcm-log-%F-%T &
 lcm-spy &
 # GPS Driver
 ./gps_driver /dev/ttyUSB0
+#./send-message.py 
 kill %1 %2 #%3 #%4%5%6%7%8%9
